6017115a76dc0dc1d3cd2963305717950a474ae6
Md.Antonin Islam <md.antonin6862@gmail.com>
2018-11-04 18:33:00 +0600

    add

--- pages/addProduct.php
+++ pages/addProduct.php
@@ -1,29 +1,33 @@
 <?php
     session_start();
-
+    //Check if logged in
     if(empty($_SESSION['activeUser']))
     {
         header("location:register.php");
-    }
-    else
+    }else
     {
-        $user = $_SESSION['activeuser'];
-        $file = "../xml/".$user.".xml";
-
+        //check if submit
         if ($_SERVER["REQUEST_METHOD"] == "POST")
         {
+            $user = $_SESSION['activeUser'];
+            $file = "../xml/products.xml";
+            $imgPath = "../uploads/" . basename($_FILES["ppic"]["name"]);
             $name = $_POST["pname"];
             $price = $_POST["pprice"];
             $pid = $_POST["pid"];
             $quantity = $_POST["pquantity"];
+            uploadImg();
             $xml = simplexml_load_file($file);
 
             $product = $xml->addChild('product');
-            $product->addChild('name', $name);
+            $product->addChild('username', $user);
+            $product->addChild('fullname', $name);
+            $product->addChild('img', $imgPath);
             $product->addChild('pid', $pid);
             $product->addChild('price', $price);
             $product->addChild('quantity', $quantity);
             $product->addChild('status', "pending");
+            
 
             $dom = new DOMDocument('1.0');
             $dom->preserveWhiteSpace = false;
@@ -31,7 +35,57 @@
             $dom->loadXML($xml->asXML());
             $dom->save($file);
 
-            header("location:seller.php");
+           header("location:seller.php");
+        }
+
+        
+    }
+
+    function uploadImg()
+    {
+        $target_dir = "../uploads/";
+        $target_file = $target_dir . basename($_FILES["ppic"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        
+            $check = getimagesize($_FILES["ppic"]["tmp_name"]);
+            if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+        // Check file size
+        if ($_FILES["ppic"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadOk = 0;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["ppic"]["tmp_name"], $target_file)) {
+               // echo "The file ". basename( $_FILES["ppic"]["name"]). " has been uploaded.";
+                //echo $target_file;
+                
+            } else {
+               // echo "Sorry, there was an error uploading your file.";
+            }
         }
     }
     
@@ -46,7 +100,7 @@
 <body>
     
     <div class="formbox">
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="POST" enctype="multipart/form-data">
         <table>
             <tr>
                 <td>Name:</td>
@@ -64,8 +118,13 @@
             </tr>
 
             <tr>
-                <td>Product ID:</td>
+                <td>Quantity:</td>
                 <td><input type="text" name="pquantity" value="5"></td>
+            </tr>
+
+            <tr>
+                <td>Upload Image:</td>
+                <td><input type="file" name="ppic" id="ppic" accept="image/*"></td>
             </tr>
 
             <tr>
--- pages/seller.php
+++ pages/seller.php
@@ -1,14 +1,14 @@
 <?php 
     session_start();
-
+    $user = "";
     if(empty($_SESSION['activeUser']))
     {
         header("location:register.php");
     }
     else
     {
-        $user = $_SESSION['activeuser'];
-        $file = "../xml/".$user.".xml";
+        $user = $_SESSION['activeUser'];
+        $file = "../xml/products.xml";
     }
 ?>
 
@@ -24,6 +24,7 @@
         <a href="addProduct.php" class="button">Add Product</a>
             <tr>
                 <td>Name</td>
+                <td>Image</td>
                 <td>Product ID</td>
                 <td>Price</td>
                 <td>Quantity</td>
@@ -34,13 +35,17 @@
 
                 foreach($dom->product as $p)
                 {
-                    echo "<tr>";
-                        echo "<td>".$p->name."</td>";
+                    if($p->username == $user)
+                    {
+                        echo "<tr>";
+                        echo "<td>".$p->fullname."</td>";
+                        echo "<td><img style='width: 80px;height:80px' src='".$p->img."'></td>";
                         echo "<td>".$p->pid."</td>";
                         echo "<td>".$p->price."</td>";
                         echo "<td>".$p->quantity."</td>";
                         echo "<td>".$p->status."</td>";
                     echo "</tr>";
+                    }         
                 }
             ?> 
         </table>    
